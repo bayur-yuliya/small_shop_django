808c34784752a445df3026b2dcab7718741a5b7e
Yuliya Bayur <juliy14497@gmail.com>
2023-11-28 20:41:10 +0200

    Add: registration

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
@@ -7,4 +8,6 @@ urlpatterns = [
     path("categories/", views.categories, name="categories"),
     path("products_list/<int:categories_id>", views.list_by_products, name="products"),
     path("product/<int:num_product>", views.product, name="one_product"),
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 
+from catalog.forms import RegisterUserForm
 from catalog.models import ProductCatalog, Product
 
 
-def index(request):
+def register(request):
+    if request.method == "GET":
+        form = RegisterUserForm()
+        return render(request, "catalog/register.html", {"form": form})
 
+    form = RegisterUserForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return render(request, "catalog/register.html", {"form": form})
+
+
+def index(request):
     catalogs = ProductCatalog.objects.all()
     products = Product.objects.all()
-    return render(request, "catalog/index.html", {'catalogs': catalogs, 'products': products})
+    return render(
+        request, "catalog/index.html", {"catalogs": catalogs, "products": products}
+    )
 
 
 def categories(request):
     catalogs = ProductCatalog.objects.all()
-    return render(request, "catalog/categories.html", {'catalogs': catalogs})
+    return render(request, "catalog/categories.html", {"catalogs": catalogs})
 
 
 def list_by_products(request, categories_id):
     catalogs = get_object_or_404(ProductCatalog, id=categories_id)
     products = Product.objects.filter(category=catalogs.id)
 
-    return render(request, "catalog/list_by_products.html", {'products': products})
+    return render(request, "catalog/list_by_products.html", {"products": products})
 
 
 def product(request, num_product):
     products = Product.objects.get(id=num_product)
-    return render(request, "catalog/product.html", {'products': products})
+    return render(request, "catalog/product.html", {"products": products})
