dbcde0a3abc8fe4322217624212460af9d2f7f35
Yuliya Bayur <juliy14497@gmail.com>
2024-02-27 17:13:16 +0200

    resolve conflict

--- shop/cart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.cart, name="cart"),
-    path("order/<int:num_order>", views.create_order, name="order"),
-]
--- shop/cart/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def create_order(request, num_order):
-    return render(request, "cart/create_order.html")
-
-
-def cart(request):
-    return render(request, "cart/cart.html")
